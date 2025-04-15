be002969b222e3bfc600a5728774bc757425850a
Chris <xhamanichristian@gmail.com>
2025-04-15 16:12:22 +0300

    sters todo

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,7 +1,7 @@
 #include "../includes/TestRunner.h"
 
 int main() {
-    // TODO: daca dau o camera care nu exista da crash
+
     Hotel hotel;
     TestRunner::RunInteractiveMenu(hotel);
     return 0;
